b3687375cf78eb2688a082bd8949d2be9e7e1d88
faizkhan70 <fk3574533@gmail.com>
2024-12-20 14:36:26 +0530

    change commit

--- src/pages/page4/Page4.jsx
+++ src/pages/page4/Page4.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Page4 = () => {
   return (
     <section className="bg-black text-white py-16 px-6">
-    <div className="max-w-7xl mx-auto flex flex-col lg:flex-row justify-between items-center gap-12">
+    <div className="max-w-7xl mx-auto flex flex-col lg:flex-row justify-center items-center gap-12">
       {/* Left Side - Grid Box */}
       <div className="grid grid-cols-1 md:grid-cols-2 gap-8 lg:w-1/2">
         <div className="flex flex-col items-center px-6 py-4 border border-gray-400 hover:bg-gray-700 text-sm tracking-widest uppercase">
@@ -23,11 +23,11 @@ const Page4 = () => {
       </div>
   
       {/* Right Side - Contact Form */}
-      <div className="lg:w-1/2">
-        <h2 className="text-3xl lg:text-4xl font-bold mb-8 text-center lg:text-left">
+      <div className="lg:w-1/2 flex flex-col items-center">
+        <h2 className="text-3xl lg:text-4xl font-bold mb-8 text-center">
           Let's Get In Touch
         </h2>
-        <form className="grid grid-cols-1 md:grid-cols-2 gap-6">
+        <form className="grid grid-cols-1 md:grid-cols-2 gap-6 w-full">
           <div className="flex flex-col">
             <label className="font-semibold mb-2" htmlFor="full-name">
               Full Name <span className="text-red-500">*</span>
@@ -96,6 +96,7 @@ const Page4 = () => {
     </div>
   </section>
   
+
   );
 };
 
